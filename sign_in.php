9a7968b70cd6302050724ab8c148dc8825961c48
Michael Rdoriguez <maicolrdgz123@gmail.com>
2026-05-22 12:05:38 -0500

    login galeras cars

--- sign_in.php
+++ sign_in.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if(isset($_SESSION['user_id'])) {
+    header('refresh:0; url=index.php');
+}
+
 include __DIR__ . '/config/database.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -16,15 +21,19 @@ if (!$email || !$password) {
 
 $enc_pass = md5($password);
 
-$sql_login = 'SELECT u.id, u.firstname, u.lastname FROM users u WHERE u.email = $1 AND u.password = $2';
-$res_login = pg_query_params($local_conn, $sql_login, [$email, $enc_pass]);
+$sql_login = "SELECT u.id, u.email, u.firstname || ' ' || u.lastname AS full_name FROM users u WHERE u.email = '$email' AND u.password = '$enc_pass'";
+$res_login = pg_query_params($local_conn, $sql_login, []);
 
 if (!$res_login) {
+    $row = pg_fetch_assoc($res_login);
     echo 'Error en la consulta de login.';
     exit;
 }
 
 if (pg_num_rows($res_login) > 0) {
+    $_SESSION['user_id'] = $row['id'];
+    $_SESSION['user_email'] = $row['email'];
+    $_SESSION['user_name'] = $row['full_name'];
     header('Location: index.html');
     exit;
 }
